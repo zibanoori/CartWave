3cbc99d09439d6058acc87010a7d7b10814eed31
zibanoori <noorizibaa@gmail.com>
2026-09-26 20:26:16 +0330

    Rename database file to cartwave.sqlite3 and apply initial migrations

--- cartwave_project/settings.py
+++ cartwave_project/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'cartwave_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'cartwave.sqlite3',
     }
 }
 
