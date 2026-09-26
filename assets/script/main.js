c4e2bcccaa4855da7e95706a09117acf86a861ee
zibanoori <noorizibaa@gmail.com>
2026-09-26 14:24:07 +0330

    Fix typo in localStorage and finalize dark mode toggle logic

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,7 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const themeToggleBtn = document.querySelector('.js-theme-toggle');
   const htmlElement = document.documentElement;
   const icon = themeToggleBtn?.querySelector('i');
+
   const savedTheme = localStorage.getItem('them') || 'light'
   htmlElement.setAttribute('data-theme', savedTheme);
-  updateTcon(savedTheme);
+  updateIcon(savedTheme);
+
+  themeToggleBtn?.addEventListener('click', () => {
+    const currentTheme = htmlElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    htmlElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+    updateIcon(newTheme);
+  });
 });
