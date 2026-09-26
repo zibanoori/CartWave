573900a7094f2478fa10d99ae9effa7e0fdcbae9
zibanoori <noorizibaa@gmail.com>
2026-09-26 13:42:28 +0330

    Fix typo in theme key and ensure persistent dark mode

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const themeToggleBtn = document.querySelector('.js-theme-toggle');
   const htmlElement = document.documentElement;
   const icon = themeToggleBtn?.querySelector('i');
+  const savedTheme = localStorage.getItem('them') || 'light'
+  htmlElement.setAttribute('data-theme', savedTheme);
+  updateTcon(savedTheme);
 });
