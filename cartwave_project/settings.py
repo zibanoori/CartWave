e728abc5e1c8f2f95ab5abb6f4f50cf5faca4ddd
zibanoori <noorizibaa@gmail.com>
2026-09-26 18:23:04 +0330

    Configure installed apps and static files settings

--- cartwave_project/settings.py
+++ cartwave_project/settings.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = 'cartwave_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,6 +101,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 # Email
